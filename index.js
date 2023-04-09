bf989040460bbb15bfc26d392607b09b1dcfb5a4
Jayloneal <121124684+Jayloneal@users.noreply.github.com>
2023-04-09 08:34:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
